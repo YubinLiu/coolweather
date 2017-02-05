f589fb08dcf2b14c7c9f4552b815853de79ca66b
Liuyubin <lybin0904@gmail.com>
2017-02-05 16:42:58 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by yubin on 2017/2/4.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
